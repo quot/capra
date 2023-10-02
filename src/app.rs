ed6d7f95918abd6fc330c50d3643e51f2d1914b3
Alex Coté <alex.cote@tuta.io>
2023-10-02 14:08:42 -0500

    Clean up; Add sidebar

--- src/app.rs
+++ src/app.rs
@@ -43,6 +43,7 @@ impl eframe::App for MainApp {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        ctx.set_pixels_per_point(1.1);
         // Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
@@ -50,23 +51,31 @@ impl eframe::App for MainApp {
             // The top panel is often a good place for a menu bar:
 
             egui::menu::bar(ui, |ui| {
-                #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
-                {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            _frame.close();
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
-
-                egui::widgets::global_dark_light_mode_buttons(ui);
+                ui.menu_button("File", |ui| {
+                    if ui.button("Quit").clicked() {
+                        _frame.close();
+                    }
+                });
+                ui.separator();
+                egui::widgets::global_dark_light_mode_switch(ui);
             });
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+            egui::SidePanel::left("requests_panel").show_inside(ui, |ui| {
+                ui.heading("Requests");
+                ui.separator();
+                ui.label("Request 0");
+                ui.collapsing("Directory Test", |ui| {
+                    ui.label("Request 1");
+                    ui.label("Request 2");
+
+                    ui.collapsing("Directory Test 2", |ui| {
+                        ui.label("Request 1");
+                        ui.label("Request 2");
+                    });
+                });
+            });
 
             ui.horizontal(|ui| {
                 ui.label("Write something: ");
@@ -80,29 +89,9 @@ impl eframe::App for MainApp {
 
             ui.separator();
 
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
                 egui::warn_if_debug_build(ui);
             });
         });
     }
-}
-
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
 }
